1ed6ad65a5fb1f5d0e5014279a6b210004e93d9c
sidd <sidd.1105@gmail.com>
2016-11-07 17:28:58 +0530

    quiz.py

---
+++ quiz.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+if age >= 18:
+ print("Welcome aboard " + name + ",You have been missed") 
+else:
+  print("Permission Denied young one!")
